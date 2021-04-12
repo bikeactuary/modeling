d1f0e418b170e995bb6cb86b16d9d31f6479ef95
bikeactuary <mike@michael-barr.com>
2021-04-12 09:34:59 -0400

    Bayesian MBO blog post

---
+++ bayesianMBO/publish.R
@@ -0,0 +1,14 @@
+library(RWordPress)
+library(knitr)
+
+options(WordpressLogin = c(mike = readLines("~/wp_login.txt")),
+        WordpressURL = 'https://michael-barr.com/xmlrpc.php')
+
+knit2wp('~/repos/my_site/modeling/bayesianMBO/bayesianMBO.Rmd', title = 'test Bayesian Model Based Optimization in R',
+        categories = c("Data Science"),
+        # tags = c("R", "xgboost", "mlrMBO", "gaussian processes", "bayes", "workplace efficiency", "tweedie"),
+        shortcode = c(TRUE,TRUE),
+        # action = "newPost",
+        # action = "editPost",
+        # postid = 422,
+        publish = FALSE)

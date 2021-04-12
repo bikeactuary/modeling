5411bebf7fbf09332628149f8a097c8990ba77df
bikeactuary <mike@michael-barr.com>
2021-04-12 09:35:44 -0400

    replace utf8encode plot imgs for r-bloggers

--- bayesianMBO/publish.R
+++ bayesianMBO/publish.R
@@ -4,11 +4,11 @@ library(knitr)
 options(WordpressLogin = c(mike = readLines("~/wp_login.txt")),
         WordpressURL = 'https://michael-barr.com/xmlrpc.php')
 
-knit2wp('~/repos/my_site/modeling/bayesianMBO/bayesianMBO.Rmd', title = 'test Bayesian Model Based Optimization in R',
+knit2wp('~/repos/my_site/modeling/bayesianMBO/bayesianMBO.Rmd', title = 'Bayesian Model Based Optimization in R',
         categories = c("Data Science"),
         tags = c("R", "xgboost", "mlrMBO", "gaussian processes", "bayes", "workplace efficiency", "tweedie"),
         shortcode = c(TRUE,TRUE),
-        action = "newPost",
-        # action = "editPost",
-        # postid = 422,
-        publish = FALSE)
+        # action = "newPost",
+        action = "editPost",
+        postid = 422,
+        publish = TRUE)

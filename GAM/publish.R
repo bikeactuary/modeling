40cfffa7ee56438dc1736793073d88afc3c174ab
bikeactuary <mike@michael-barr.com>
2021-04-12 10:11:02 -0400

    update post

--- GAM/publish.R
+++ GAM/publish.R
@@ -4,8 +4,9 @@ library(knitr)
 options(WordpressLogin = c(mike = readLines("~/wp_login.txt")),
         WordpressURL = 'https://michael-barr.com/xmlrpc.php')
 
-knit2wp('~/repos/modeling/gams_scams.Rmd', title = 'GAMs and scams: Part 1',
+knit2wp('~/repos/my_site/modeling/GAM/gams_scams.Rmd', title = 'GAMs and scams: Part 1',
         categories = c("Data Science", "Insurance"),
-        tags = c("r", "mgcv", "GAM", "scam", "modeling"),
-        # action = "newPost",
-        publish = FALSE)
+        tags = c("r", "mgcv", "GAM", "scam", "modeling", "pricing"),
+        action = "editPost",
+        postid = 327,
+        publish = TRUE)
